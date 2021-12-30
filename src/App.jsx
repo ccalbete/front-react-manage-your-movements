2abae1205835a2129c6b4a95b71600ec8666c1e4
ccalbete <ccalbete10@gmail.com>
2021-12-30 15:46:08 -0300

    show text from app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (<>
+        <p>test</p></>);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-function MostrarHola({ texto, segundoTexto, children }) {
-  return (<>
-    <p>mostrar Hola</p>
-    <p>texto</p>
-    <p>segundoTexto</p>
-    {children}
-  </>);
-}
-
-ReactDOM.render(
-  <div>
-    <MostrarHola texto="Primer texto" segundoTexto="Segundo"><p>"prueba"</p></MostrarHola>
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
