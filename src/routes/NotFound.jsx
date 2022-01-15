93e59261780d6a63d41c878a684bb2ebfaa5268e
ccalbete <ccalbete10@gmail.com>
2022-01-15 01:19:40 -0300

    improve page not found

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -6,11 +6,14 @@ import routePaths from './routePaths';
 const NotFound = () => {
     return (
         <>
-            <span >404 Not Found</span>
-            <br />
-            <Link to={routePaths.home} >
-                Go back to home
-            </Link>
+            <p style={{ textAlign: 'center', fontSize: '22px', marginTop: '50px' }}>
+                404 Page not found
+                <br />
+                <Link to={routePaths.login} >
+                    Go back to login
+                </Link> </p>
+
+
         </>
     )
 };
